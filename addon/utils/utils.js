9e230d36cf9c19378c9d7ce42d2d871fbdb768dd
rrglomsk <rachelglomski@eflexsystems.com>
2025-06-30 16:06:02 -0400

    no ember-data import

--- addon/utils/utils.js
+++ addon/utils/utils.js
@@ -1,10 +1,26 @@
 import { isArray } from '@ember/array';
-// eslint-disable-next-line ember/use-ember-data-rfc-395-imports
-import DS from 'ember-data';
 import Model from '@ember-data/model';
+import {
+  macroCondition,
+  dependencySatisfies,
+  importSync,
+} from '@embroider/macros';
+
+let DS;
+if (macroCondition(dependencySatisfies('ember-data', '*'))) {
+  DS = importSync('ember-data').default;
+}
 
 export function isValidatable(value) {
   return value && value instanceof Model ? !value.isDeleted : true;
+}
+
+export function isDSManyArray(o) {
+  return !!(
+    DS &&
+    o &&
+    (o instanceof DS.PromiseManyArray || o instanceof DS.ManyArray)
+  );
 }
 
 export function getValidatableValue(value) {
@@ -12,7 +28,7 @@ export function getValidatableValue(value) {
     return value;
   }
 
-  if (value && isArray(value) && value instanceof DS.ManyArray) {
+  if (value && isArray(value) && value && isDSManyArray(value)) {
     return value.filter((v) => isValidatable(v));
   }
 
