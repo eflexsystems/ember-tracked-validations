72d7387cce0313a86c93aba9ced0e9ea61098b41
rrglomsk <rachelglomski@eflexsystems.com>
2025-07-01 14:14:05 -0400

    update

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,6 +12,12 @@ module.exports = function (defaults) {
       importBootstrapFont: false,
       importBootstrapCSS: false,
     },
+
+    'emberData': {
+      deprecations: {
+        DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false
+      }
+    },
   });
 
   /*
